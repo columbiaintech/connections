4a3c521cb7cbe22bae42f050a2225de9e8451d9d
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-02-17 22:03:37 -0500

    date picker components, simple event form

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,3 +1,29 @@
 "use client";
-
+import styles from "../app/styles/Home.module.css"
+import DatePicker from "./DatePicker"
 // TODO: get event details as textinput: event name, location, date, time
+
+interface EventFormProps {
+    eventName: string;
+}
+
+function EventForm(props: EventFormProps) {
+    return (
+        <div>
+            <form className={styles.eventForm}>
+                <textarea name="eventName"
+                          spellCheck="false"
+                          autoCapitalize="words"
+                    onInput={(e) => {
+                        e.target.style.height = '';
+                        e.target.style.height = e.target.scrollHeight + 'px';
+                    }}
+                          className="font-[family-name:var(--font-geist-mono)] text-3xl"
+                          placeholder={"Event Name"}
+                />
+            <DatePicker/>
+            </form>
+        </div>
+    );
+};
+export default EventForm;
