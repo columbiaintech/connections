07d8879a86c20736eca4501fee82e5a569957d0f
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-05-25 17:50:14 -0400

    simplify, remove err message

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,62 +1,35 @@
 "use client";
 import React from "react";
-import {useState, FormEvent} from "react";
-// TODO: flesh out auth
-
-interface SignInFormProps {
-    dbColumns: string[];
-}
-
-interface SignInFormData{
-    eventName: string;
-    eventDate: Date | null;
-    mappedData: Record<string, any>[];
-}
+import {signin} from "../app/actions/auth"
 
 function SignInForm() {
 
-    async function handleSubmit(event:FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        setIsLoading(true);
-        setError(null);
-    }
-
-    const [emailAddress, setEmailAddress] = useState('');
-
-
-
-
     return (
         <div className="w-full px-4 sm:px-0 mx-auto text-base">
-            <form className="text-gray-800 px-6 py-6 flex flex-col gap-4 items-start max-w-full"  onSubmit={handleSubmit}>
-
+            <form className=" shadow-sm sm:rounded-lg bg-style2 text-gray-800 px-6 py-6 flex flex-col gap-4 items-start max-w-full">
                 <div className="flex flex-col w-full">
-
                     <div className="flex items-center gap-4 pb-4 w-full flex-row">
                         <div className="w-full">
-
-                            <p className="text-gray-500">Email address</p>
-                            <input name="emailAddress" value={emailAddress} onChange={(e)=>setEmailAddress(e.target.value)}
+                            <label htmlFor="emailAddress" className="text-gray-500">Email address</label>
+                            <input id="emailAddress" name="emailAddress"
                                    spellCheck="false"
-                                   autoCapitalize="words"
+                                   autoCapitalize="none"
                                    className="w-full ps-2 py-1 text-gray-800
-                            font-[family-name:var(--font-sourceSans3)] text-l
-                           outline-2 outline-offset-2 outline-solid
-                           focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
-                                                      border border-solid border-gray-200
-
-                           "
-                                   placeholder={"Email address"}
+                                   font-[family-name:var(--font-sourceSans3)] text-l
+                                   outline-2 outline-offset-2 outline-solid
+                                   focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
+                                                              border border-solid border-gray-200
+                                    "
+                                   placeholder={"Email address"} required
                             />
                         </div>
                     </div>
-                    <div className="items-stretch
- flex items-center gap-4 pb-4 w-full flex-row">
+                    <div className="items-stretch flex items-center gap-4 pb-4 w-full flex-row">
                         <div className="w-full">
-                            <p className="text-gray-500">Password</p>
+                            <label htmlFor="password" className="text-gray-500">Password</label>
                             <input id="password" name="password" type="password"
                                    className="w-full ps-2 py-1 text-gray-800
-                            font-[family-name:var(--font-sourceSans3)] text-l
+                           font-[family-name:var(--font-sourceSans3)] text-l
                            outline-2 outline-offset-2 outline-solid
                            focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
                            border border-solid border-gray-200
@@ -67,16 +40,23 @@ function SignInForm() {
 
                     <button
                         type="submit"
-                        className="text-s bg-sea-600 hover:bg-sea-500 text-white rounded-sm px-2 py-1 cursor-pointer font-[family-name:var(--font-fragment-mono)]">
-
-                        {'Log In'}
+                        formAction={signin}
+                        className="
+                          text-s text-white rounded-sm px-2 py-1
+                          flex items-center gap-4
+                          justify-center
+                          rounded-lg border border-sea-600
+                          bg-gradient-to-r from-sea-600 to-sea
+                          hover:opacity-90
+                          shadow-[0_2px_0] shadow-sea
+                          transition-all duration-200 ease-in-out
+                          cursor-pointer font-[family-name:var(--font-fragment-mono)]"
+                    >
+                        Sign In
                     </button>
                 </div>
                 <p className="text-gray-500">Don't have an account? <a href="/signup" className="text-sea-600 hover:text-sea-500 font-semibold">Sign up</a></p>
-
             </form>
-            {<p className="text-red-500">{}</p>}
-
         </div>
     );
 }
