22e5ca5afb80ba1b8045714c3d9e638ab5ef8ca8
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-05-25 17:54:26 -0400

    clean up, use server action

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,54 +1,75 @@
 "use client";
 import React from "react";
-import {useState, FormEvent} from "react";
-// TODO: flesh out auth
-
-interface SignUpFormProps {
-    dbColumns: string[];
-}
-
-interface SignUpFormData{
-    eventName: string;
-    eventDate: Date | null;
-    mappedData: Record<string, any>[];
-}
+import {useState} from "react";
+import {signup} from "../app/actions/auth.ts"
 
 function SignUpForm() {
-
-    async function handleSubmit(event:FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        setIsLoading(true);
-        setError(null);
-    }
-
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isSuccess, setIsSuccess] = useState(false);
 
+    const handleSignUp = async (formData) => {
+        setError('');
+        setIsLoading(true);
 
+        try{
+                const emailAddress = formData.get('emailAddress');
+                const password = formData.get('password');
+                const firstName = formData.get('firstName');
+                const lastName = formData.get('lastName');
+                if (!firstName || !lastName || !emailAddress || !password) {
+                    setError('Please fill out all fields');
+                    setIsLoading(false);
+                    return;
+                }
+                const result = await signup(formData);
+                if (result?.error) {
+                    setError(result.error.message);}
+                else{
+                    setIsSuccess(true);
+                }
+            } catch(e){
+                setError(e.message||'An error has occurred. Please try again.')
+            } finally{
+            setIsLoading(false);
+        }
+    };
 
+    if (isSuccess) {
+        return (
+            <div className="max-w-xl px-4 sm:px-0 mx-auto text-base w-full">
+                <div className="shadow-sm sm:rounded-lg bg-style2 text-gray-800 px-6 py-6 flex flex-col gap-4 items-start max-w-full">
+                    <h2 className="text-xl font-semibold text-gray-800">Check your email</h2>
+                    <p className="text-gray-600">
+                        We've sent a confirmation link to your email address. Please check your inbox and click the link to confirm your account.
+                    </p>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="max-w-xl px-4 sm:px-0 mx-auto text-base w-full">
-            <form className=" shadow-sm sm:rounded-lg bg-style2 text-gray-800 px-6 py-6 flex flex-col gap-4 items-start max-w-full"  onSubmit={handleSubmit}>
-
-<div className="flex-row">
-                <div className="flex items-center gap-4 pb-4 w-full">
-                    <div className="flex-col">
-                <p className="text-gray-500">First name</p>
-                <input name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)}
-                       spellCheck="false"
-                       autoCapitalize="words"
-                       className="ps-2 py-1 text-gray-800
-                        font-[family-name:var(--font-sourceSans3)] text-l
-                       outline-2 outline-offset-2 outline-solid
-                       focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
-                           border border-solid border-gray-200
-
-"
-                       placeholder={"First name"}
-                />
-                 </div>
+            <form action={handleSignUp} className=" shadow-sm sm:rounded-lg bg-style2 text-gray-800 px-6 py-6 flex flex-col gap-4 items-start max-w-full">
+                <div className="flex-row">
+                    <div className="flex items-center gap-4 pb-4 w-full">
+                        <div className="flex-col">
+                        <p className="text-gray-500">First name</p>
+                        <input name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)}
+                               spellCheck="false"
+                               autoCapitalize="words"
+                               className="ps-2 py-1 text-gray-800
+                               font-[family-name:var(--font-sourceSans3)] text-l
+                               outline-2 outline-offset-2 outline-solid
+                               focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
+                               border border-solid border-gray-200"
+                               placeholder={"First name"}
+                            />
+                         </div>
 
                     <div className="flex-col ">
                         <p className="text-gray-500">Last name</p>
@@ -57,21 +78,18 @@ function SignUpForm() {
                                spellCheck="false"
                                autoCapitalize="words"
                                className="ps-2 py-1 text-gray-800
-
-                        font-[family-name:var(--font-sourceSans3)] text-l
-                       outline-2 outline-offset-2 outline-solid
-                       focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
-                                                  border border-solid border-gray-200
-
-                       "
+                               font-[family-name:var(--font-sourceSans3)] text-l
+                               outline-2 outline-offset-2 outline-solid
+                               focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
+                               border border-solid border-gray-200
+                               "
                                placeholder={"Last name"}
-                               />
+                        />
                 </div>
                 </div>
 
     <div className="flex items-center gap-4 pb-4 w-full flex-col">
         <div className="w-full">
-
         <p className="text-gray-500">Email address</p>
         <input name="emailAddress" value={emailAddress} onChange={(e)=>setEmailAddress(e.target.value)}
                spellCheck="false"
@@ -80,41 +98,47 @@ function SignUpForm() {
                             font-[family-name:var(--font-sourceSans3)] text-l
                            outline-2 outline-offset-2 outline-solid
                            focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
-                                                      border border-solid border-gray-200
-
+                           border border-solid border-gray-200
                            "
                placeholder={"Email address"}
         />
     </div>
     </div>
     <div className="items-stretch
- flex items-center gap-4 pb-4 w-full flex-col">
-    <div className="w-full">
-    <p className="text-gray-500">Password</p>
-    <input id="password" name="password" type="password"
-           className="w-full ps-2 py-1 text-gray-800
-                            font-[family-name:var(--font-sourceSans3)] text-l
-                           outline-2 outline-offset-2 outline-solid
-                           focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
-                           border border-solid border-gray-200
-                           "
-           placeholder={"Enter password"}/>
+        flex items-center gap-4 pb-4 w-full flex-col">
+        <div className="w-full">
+        <p className="text-gray-500">Password</p>
+        <input id="password" name="password" type="password"
+               value={password} onChange={(e)=>setPassword(e.target.value)}
+               className="w-full ps-2 py-1 text-gray-800
+                                font-[family-name:var(--font-sourceSans3)] text-l
+                               outline-2 outline-offset-2 outline-solid
+                               focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-gray-200 rounded-sm
+                               border border-solid border-gray-200
+                               "
+               placeholder={"Enter password"}/>
         </div>
     </div>
 
     <button
-                    type="submit"
-                    className="text-s bg-sea-600 hover:bg-sea-500 text-white rounded-sm px-2 py-1 cursor-pointer font-[family-name:var(--font-fragment-mono)]">
-
-                    {'Create Account'}
-                </button>
-
-            </div>
+            type="submit" disabled={isLoading}
+        className="
+                          text-s text-white rounded-sm px-2 py-1
+                          flex items-center gap-4
+                          justify-center
+                          rounded-lg border border-sea-600
+                          bg-gradient-to-r from-sea-600 to-sea
+                          hover:opacity-90
+                            shadow-[0_2px_0] shadow-sea
+                          transition-all duration-200 ease-in-out
+                          cursor-pointer font-[family-name:var(--font-fragment-mono)]"
+    >
+        {isLoading? 'Signing Up...':'Sign Up'}
+    </button>
+</div>
                 <p className="text-gray-500">Already have an account? <a href="/signin" className="text-sea-600 hover:text-sea-500 font-semibold">Sign in</a></p>
-
             </form>
-            {<p className="text-red-500">{}</p>}
-
+            {<p className="text-red-500">{error}</p>}
         </div>
     );
 }
