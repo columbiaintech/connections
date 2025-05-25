b77a25a6f4058ab7b6bd0ec9a48b184769e58e71
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-05-25 17:59:58 -0400

    fix styling, import navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,26 @@
 
 import EventForm from "../components/EventForm";
 import {fetchColumns} from "@/app/actions/updateData";
-import Image from "next/image";
-import RiveEmbed from "@/components/RiveEmbed";
+import Navbar from "../components/Navbar"
 
 export default async function Home() {
     const dbColumns = await fetchColumns();
-
-  return (
-      <div className="bg-style1 text-teal bg-cover bg-no-repeat bg-center min-h-screen grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sourceSans3)]">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="flex items-center gap-2">
-              {/*<Image*/}
-              {/*    className="dark:invert"*/}
-              {/*    src="/connections-color.svg"*/}
-              {/*    alt="Connections logo mark"*/}
-              {/*    width={55}*/}
-              {/*    height={38}*/}
-              {/*    priority*/}
-              {/*/>*/}
-              <RiveEmbed/>
-              <div className="text-5xl font-[family-name:var(--font-sourceSans3)] font-semibold">Connections</div>
+    return (
+      <div className="w-screen h-screen bg-style1 text-teal bg-cover bg-no-repeat bg-center">
+          <div className="p-4">
+            <Navbar />
           </div>
-        <EventForm dbColumns={dbColumns}/>
-      </main>
+
+          <div className="max-w-xl mx-auto sm:pt-12">
+              <div className="grid grid-rows-[20px_1fr_20px] justify-items-center">
+                  <main className="flex flex-col items-center sm:items-start">
+                      <div
+                  className=" bg-style2 bg-cover bg-no-repeat bg-center list-inside text-sm sm:text-left shadow-sm sm:rounded-lg">
+                          <EventForm dbColumns={dbColumns}/>
+                      </div>
+                  </main>
+              </div>
+          </div>
     </div>
   );
 }
