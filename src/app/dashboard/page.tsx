1a1b3147f73cd2d49b40a3591a3723fe08876bf9
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-15 16:05:48 -0400

    move data fetching into server component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,11 @@
 "use server"
-import { redirect } from 'next/navigation'
-import { createClient } from '../../../utils/supabase/server'
 import React from "react";
-import GroupInfo from "@/components/GroupInfo";
-import { userHasGroups, fetchUserGroupDetails } from "@/app/actions/updateData";
-import Link from "next/link";
 import DashboardDisplay from "@/components/DashboardDisplay";
 
-export default async function Dashboard() {
-    const supabase = await createClient()
-
-    const { data:userData, error } = await supabase.auth.getUser()
-    if (error || !userData?.user) {
-        redirect('/signin')
-    }
-
-    const user = userData.user;
-    const hasGroup = await userHasGroups(user.id);
-    const group = hasGroup ? await fetchUserGroupDetails(user.id) : null;
-
+export default async function Dashboard({searchParams}) {
     return (
         <div className="p-8">
-            <DashboardDisplay user={userData.user} />
+            <DashboardDisplay searchParams={searchParams}/>
         </div>
     );
 }
