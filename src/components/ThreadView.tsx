cea0477a1fe18c24e8e45ddba863f4cff14a433b
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-16 15:10:32 -0400

    basic thread view

---
+++ src/components/ThreadView.tsx
@@ -0,0 +1,23 @@
+'use server'
+import {fetchConnectionThread} from "@/app/actions/updateData";
+
+export default async function ThreadView({ connectionId }: { connectionId: string }) {
+    const thread = await fetchConnectionThread(connectionId);
+
+    if (!thread.length) return (
+        <div className="card-pink p-6">
+            <p className="text-lg text-steel">No emails found for this connection.</p>
+        </div>
+    );
+
+    return(
+        <div>
+        {thread.map((msg)=>(
+            <div key={msg.created_at}>
+                <p>{msg.sender_email}</p>
+                <div dangerouslySetInnerHTML={{__html: msg.body}} className="text-steel" />
+            </div>
+                ))}
+        </div>
+    );
+}
