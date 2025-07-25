4eca007d14cd8400c1b5cd6f404df4ad2f297910
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-25 19:17:02 -0400

    brief summary for landing, edit later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use server";
 import {fetchColumns} from "@/app/actions/updateData";
+import Link from "next/link";
 
 export default async function Home() {
     const dbColumns = await fetchColumns();
@@ -10,7 +11,11 @@ export default async function Home() {
                   <main className="flex flex-col items-center sm:items-start">
                       <div
                   className=" bg-style2 bg-cover bg-no-repeat bg-center list-inside text-sm sm:text-left shadow-sm sm:rounded-lg">
-                          {/*<EventForm dbColumns={dbColumns}/>*/}
+                          <div className="card-white p-6">
+                              <p className="text-lg text-loch">Connections sets up introductions between members of the Columbia in Tech community.</p>
+                              <p className="text-sm text-steel">Learn more about Columbia in Tech <Link className="text-loch underline" href="https://www.columbiaintech.com/">here</Link>.</p>
+                          </div>
+
                       </div>
                   </main>
               </div>
