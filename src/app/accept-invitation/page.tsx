95ab49904b037e98af17a8e3c345d0a2bcb106eb
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-26 17:27:29 -0400

    make async

--- src/app/accept-invitation/page.tsx
+++ src/app/accept-invitation/page.tsx
@@ -2,7 +2,7 @@
 import {Suspense} from 'react';
 import AcceptInvitation from "@/components/AcceptInvitation";
 
-export default function AcceptInvitationPage() {
+export default async function AcceptInvitationPage() {
     return (
         <div className="w-screen h-screen bg-style1 text-teal bg-cover bg-no-repeat bg-center">
             <Suspense fallback={<div className="text-center mt-10 text-white">Loading confirmation...</div>}>
