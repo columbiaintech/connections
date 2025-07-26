18103e69c342be25ffb2cd77e896ed906f7bf1e1
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-26 17:25:19 -0400

    remove duplicate nav

--- src/app/auth/confirm/page.tsx
+++ src/app/auth/confirm/page.tsx
@@ -6,9 +6,6 @@ import {Suspense} from "react";
 export default async function ConfirmPage() {
     return (
         <div className="w-screen h-screen bg-style1 text-teal bg-cover bg-no-repeat bg-center">
-            <div className="p-4">
-                <Navbar />
-            </div>
             <Suspense fallback={<div className="text-center mt-10 text-white">Loading confirmation...</div>}>
                 <EmailConfirmation/>
             </Suspense>
