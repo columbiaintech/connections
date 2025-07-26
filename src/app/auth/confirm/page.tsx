1fc8762724585895bae2101ff13f7c684a062a19
maya :) <64454649+mayasundar@users.noreply.github.com>
2025-07-25 20:04:15 -0400

    -> server component

--- src/app/auth/confirm/page.tsx
+++ src/app/auth/confirm/page.tsx
@@ -1,18 +1,17 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import {createClient} from '@utils/supabase/client'
-import { useRouter, useSearchParams } from 'next/navigation'
+'use server'
 import Navbar from '@/components/Navbar'
 import EmailConfirmation from '@/components/EmailConfirmation'
+import {Suspense} from "react";
 
-export default function ConfirmPage() {
+export default async function ConfirmPage() {
     return (
         <div className="w-screen h-screen bg-style1 text-teal bg-cover bg-no-repeat bg-center">
             <div className="p-4">
                 <Navbar />
             </div>
-            <EmailConfirmation/>
+            <Suspense fallback={<div className="text-center mt-10 text-white">Loading confirmation...</div>}>
+                <EmailConfirmation/>
+            </Suspense>
         </div>
     )
 }
